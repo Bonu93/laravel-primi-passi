f4518818f6bd227b520442dc16ab1064444e874b
Bonu93 <luca.morucci.kb@gmail.com>
2022-01-18 14:05:17 +0100

    add route data

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        'title' => 'Hello world from Laravel',
+        'subtitle' => 'Lorem ispum dolor sit amet'
+    ]);
 });
